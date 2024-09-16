0fa33c6788e99fd71f2df5b83f5244d7132f2942
Arodefarodef <umudefedora@gmail.com>
2024-09-16 15:27:10 -0700

    dist

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const dbConfig_1 = require("./utils/dbConfig");
 const express_session_1 = __importDefault(require("express-session"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
-const PORT = 2277;
+const port = process.env.PORT;
 app.use((0, cors_1.default)({ origin: ["*"] }));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)("codifyPlatform"));
@@ -22,7 +22,8 @@ app.use((0, express_session_1.default)({
     cookie: { secure: false, maxAge: 3600 },
 }));
 (0, main_1.mainApp)(app);
-app.listen(process.env.PORT || PORT, () => {
+app.listen(port, () => {
     console.clear();
+    console.log("server running");
     (0, dbConfig_1.dbConfig)();
 });
